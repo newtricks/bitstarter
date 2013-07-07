57c05ce45d336359d23e8afcc0bc3704a13ed009
Newtricks Olddog <Newtricks.Olddog@yahoo.com>
2013-07-07 14:45:24 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var readFile = function(filename) {
+    return fs.readFileSync(filename).toString();
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
